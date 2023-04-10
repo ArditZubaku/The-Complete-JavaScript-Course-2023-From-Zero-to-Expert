5dcda3fcc2c947c685e3f1df203ba3cd44ca42b9
zubaku92 <zubaku92@gmail.com>
2023-04-10 17:42:11 +0200

    Values, variables and data types.

---
+++ JavaScript Course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,50 @@
+let js = "amazing";
+/* if (js === "amazing") alert("JavaScript is FUN!");
+ */
+// 40 + 5 + 10 + 20 - 5;
+
+console.log(40 + 5 + 10 + 20 - 5);
+
+console.log("Person");
+console.log(23);
+
+let nameOfVariable = "Variable";
+console.log(nameOfVariable);
+console.log(nameOfVariable);
+console.log(nameOfVariable);
+
+// let variable&test = "Error";
+// let function = "Error";
+
+let CONSTANT = "Constant";
+
+let $allowed = "Allowed convention";
+
+console.log($allowed);
+
+// the value holds the datatype not the variable
+true;
+
+console.log(true);
+
+// typeof operator
+console.log(typeof true);
+console.log(typeof $allowed);
+console.log(typeof CONSTANT);
+console.log(typeof 23);
+
+let test = "String";
+console.log(typeof test);
+test = false;
+console.log(typeof test);
+
+// undefined = value and datatype
+let year;
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// bug
+console.log(typeof null);
+
