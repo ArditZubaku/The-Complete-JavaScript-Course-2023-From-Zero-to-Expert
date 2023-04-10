50042df99735c3889af44191b706be816965fcd5
zubaku92 <zubaku92@gmail.com>
2023-04-10 17:51:57 +0200

    Let, const and var.

--- JavaScript Course/01-Fundamentals-Part-1/starter/script.js
+++ JavaScript Course/01-Fundamentals-Part-1/starter/script.js
@@ -1,50 +1,69 @@
-let js = "amazing";
-/* if (js === "amazing") alert("JavaScript is FUN!");
- */
-// 40 + 5 + 10 + 20 - 5;
+// let js = "amazing";
+// /* if (js === "amazing") alert("JavaScript is FUN!");
+//  */
+// // 40 + 5 + 10 + 20 - 5;
 
-console.log(40 + 5 + 10 + 20 - 5);
+// console.log(40 + 5 + 10 + 20 - 5);
 
-console.log("Person");
-console.log(23);
+// console.log("Person");
+// console.log(23);
 
-let nameOfVariable = "Variable";
-console.log(nameOfVariable);
-console.log(nameOfVariable);
-console.log(nameOfVariable);
+// let nameOfVariable = "Variable";
+// console.log(nameOfVariable);
+// console.log(nameOfVariable);
+// console.log(nameOfVariable);
 
-// let variable&test = "Error";
-// let function = "Error";
+// // let variable&test = "Error";
+// // let function = "Error";
 
-let CONSTANT = "Constant";
+// let CONSTANT = "Constant";
 
-let $allowed = "Allowed convention";
+// let $allowed = "Allowed convention";
 
-console.log($allowed);
+// console.log($allowed);
 
-// the value holds the datatype not the variable
-true;
+// // the value holds the datatype not the variable
+// true;
 
-console.log(true);
+// console.log(true);
 
-// typeof operator
-console.log(typeof true);
-console.log(typeof $allowed);
-console.log(typeof CONSTANT);
-console.log(typeof 23);
+// // typeof operator
+// console.log(typeof true);
+// console.log(typeof $allowed);
+// console.log(typeof CONSTANT);
+// console.log(typeof 23);
 
-let test = "String";
-console.log(typeof test);
-test = false;
-console.log(typeof test);
+// let test = "String";
+// console.log(typeof test);
+// test = false;
+// console.log(typeof test);
 
-// undefined = value and datatype
-let year;
-console.log(typeof year);
+// // undefined = value and datatype
+// let year;
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-// bug
-console.log(typeof null);
+// // bug
+// console.log(typeof null);
+
+// can change
+let age = 30;
+age = 31;
+
+// can not change
+const birth = 1991;
+birth = 1990;
+
+// constants should be initialized
+// const job;
+
+let isBlockScoped = true;
+var isFunctionScoped = true;
+
+
+
+// never use var, use const always besides the cases u need the variable to change
+
 
