30ebb6460963eb2d493b046e9b6d0a3099572d73
Ardit Zubaku <zubaku92@gmail.com>
2023-04-13 19:02:20 +0200

    Functions

--- JavaScript Course/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 'use strict'; // has to be the very first statement
 // strict mode forbids us from doing certain things and creates visible errors to us
-
+/* 
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -14,3 +14,25 @@ if (hasDriversLicense) {
 
 const private = 34;
 const interface = "Test";
+ */
+
+function nameOfFunc() {
+    console.log("Test function");
+}
+
+// Calling = running = invoking the function
+nameOfFunc();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+fruitProcessor(5, 0);
+const juice = fruitProcessor(0, 5);
+console.log(juice);
+console.log(fruitProcessor(1, 10));
+
+nameOfFunc(10); // since the defined function uses no parameters, it wont use the arguments at all neither
+const nameOfFunction = nameOfFunc();
+console.log(nameOfFunction); // a function that doesn't return anything, returns undefined.
