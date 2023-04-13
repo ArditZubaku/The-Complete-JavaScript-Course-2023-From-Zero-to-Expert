e4cfa86225857f11faf8ac721d49ce922f4a8437
Ardit Zubaku <zubaku92@gmail.com>
2023-04-13 22:00:12 +0200

    Arrow functions.

--- JavaScript Course/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -36,7 +36,7 @@ console.log(fruitProcessor(1, 10));
 nameOfFunc(10); // since the defined function uses no parameters, it wont use the arguments at all neither
 const nameOfFunction = nameOfFunc();
 console.log(nameOfFunction); // a function that doesn't return anything, returns undefined. */
-/////////////////////////////////////////////////////////////////
+/* /////////////////////////////////////////////////////////////////
 // Function declarations vs expressions
 
 // Function declarations are used to define functions that can be called later 
@@ -66,4 +66,36 @@ console.log(calcAge1); */
 
 // A function is just a value in JavaScript
 // Function declarations can be called above the line that they were defined
-// Function expressions can not be called above the line that they were defined
+// Function expressions can not be called above the line that they were defined */
+
+///////////////////////////////////////////////////
+// Arrow functions
+
+// Function exp. version:
+const calcAge = function thisNameDoesntMatter(birthYear) {
+    return 2023 - birthYear;
+}
+console.log(calcAge(2008));
+
+// Arrow function version(special form of func. exp):
+const calcAge3 = birthYear => 2023 - birthYear; // return happens implicitly in one-line codes
+
+// Invoking is the same:
+console.log(calcAge3(1995));
+
+// A more complex arrow function:
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`;
+}
+
+const test = (parameter1, parameter2) => {
+    // To use the 'return' keyword explicitly there has to be curly braces, in one-liners we dont use curly braces neither the keyword return 
+    return parameter1 + parameter2
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+
+// A big problem/difference with arrow functions is that they dont get a 'this' keyword. 
