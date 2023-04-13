6925b23617bf3354b05959906f0bd6d08274f733
Ardit Zubaku <zubaku92@gmail.com>
2023-04-14 01:10:07 +0200

    Reviewing functions

--- JavaScript Course/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -43,26 +43,26 @@ console.log(nameOfFunction); // a function that doesn't return anything, returns
 // Function expressions are used to define functions that can be called now
 
 // Called above the line it was defined
-console.log(calcAge1(2010));
+console.log(calccalcAge1(2010));
 
 // Function declaration
-function calcAge1(birthYear) {
+function calccalcAge1(birthYear) {
     return 2023 - birthYear;
 }
 
-const age1 = calcAge1(2002);
-console.log(age1);
+const calcAge1 = calccalcAge1(2002);
+console.log(calcAge1);
 
 // Function expression
-const calcAge2 = function (birthYear) {
+const calccalcAge2 = function (birthYear) {
     return 2023 - birthYear;
 }
 
-const age2 = calcAge2(1991);
-console.log(age2);
+const calcAge2 = calccalcAge2(1991);
+console.log(calcAge2);
 
-/* calcAge1 = "test";
-console.log(calcAge1); */
+/* calccalcAge1 = "test";
+console.log(calccalcAge1); */
 
 // A function is just a value in JavaScript
 // Function declarations can be called above the line that they were defined
@@ -73,21 +73,21 @@ console.log(calcAge1); */
 // Arrow functions
 
 // Function exp. version:
-const calcAge = function thisNameDoesntMatter(birthYear) {
+const calccalcAge = function thisNameDoesntMatter(birthYear) {
     return 2023 - birthYear;
 }
-console.log(calcAge(2008));
+console.log(calccalcAge(2008));
 
 // Arrow function version(special form of func. exp):
-const calcAge3 = birthYear => 2023 - birthYear; // return happens implicitly in one-line codes
+const calccalcAge3 = birthYear => 2023 - birthYear; // return happens implicitly in one-line codes
 
 // Invoking is the same:
-console.log(calcAge3(1995));
+console.log(calccalcAge3(1995));
 
 // A more complex arrow function:
 const yearsUntilRetirement = (birthYear, firstName) => {
-    const age = 2023 - birthYear;
-    const retirement = 65 - age;
+    const calcAge = 2023 - birthYear;
+    const retirement = 65 - calcAge;
     // return retirement;
     return `${firstName} retires in ${retirement} years.`;
 }
@@ -103,6 +103,7 @@ console.log(yearsUntilRetirement(1991, "Jonas"));
 
 */
 
+/* 
 ///////////////////////////////////////////////////
 // Functions calling other functions
 
@@ -119,5 +120,61 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(4, 8));
+ */
+
+/////////////////////////////////////////////
+// Reviewing functions
+
+const calcAge = function (birthYear) {
+    return 2023 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years.`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired.`);
+        return -1;
+    }
+
+
+}
+
+console.log(yearsUntilRetirement(2002, "FirstName"));
+console.log(yearsUntilRetirement(1950, "Old"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
