0e7b04f553ffefd9e516c3661a0cfedc5ed0b775
Ardit Zubaku <zubaku92@gmail.com>
2023-04-13 21:36:37 +0200

    Function declarations vs function expressions.

--- JavaScript Course/02-Fundamentals-Part-2/starter/script.js
+++ JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -16,7 +16,7 @@ const private = 34;
 const interface = "Test";
  */
 
-function nameOfFunc() {
+/* function nameOfFunc() {
     console.log("Test function");
 }
 
@@ -35,4 +35,35 @@ console.log(fruitProcessor(1, 10));
 
 nameOfFunc(10); // since the defined function uses no parameters, it wont use the arguments at all neither
 const nameOfFunction = nameOfFunc();
-console.log(nameOfFunction); // a function that doesn't return anything, returns undefined.
+console.log(nameOfFunction); // a function that doesn't return anything, returns undefined. */
+/////////////////////////////////////////////////////////////////
+// Function declarations vs expressions
+
+// Function declarations are used to define functions that can be called later 
+// Function expressions are used to define functions that can be called now
+
+// Called above the line it was defined
+console.log(calcAge1(2010));
+
+// Function declaration
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+
+const age1 = calcAge1(2002);
+console.log(age1);
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2023 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+console.log(age2);
+
+/* calcAge1 = "test";
+console.log(calcAge1); */
+
+// A function is just a value in JavaScript
+// Function declarations can be called above the line that they were defined
+// Function expressions can not be called above the line that they were defined
