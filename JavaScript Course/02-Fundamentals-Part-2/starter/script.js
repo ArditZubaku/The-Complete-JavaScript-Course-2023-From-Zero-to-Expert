e63616b0eeae8ecb66a1a0fef2a14f07387dbf8b
Ardit Zubaku <zubaku92@gmail.com>
2023-04-13 18:39:48 +0200

    Activating strict mode

---
+++ JavaScript Course/02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+'use strict'; // has to be the very first statement
+// strict mode forbids us from doing certain things and creates visible errors to us
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+    hasDriverLicense = true;
+}
+
+if (hasDriversLicense) {
+    console.log("I can drive :D");
+}
+
+const private = 34;
+const interface = "Test";
