6f7fcb22483734f0a3aeef4e76a3aa970a11e863
Ardit Zubaku <zubaku92@gmail.com>
2023-04-19 12:37:11 +0200

    Handling click events.

--- JavaScript Course/05-Guess-My-Number/starter/script.js
+++ JavaScript Course/05-Guess-My-Number/starter/script.js
@@ -3,6 +3,8 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 // Project #1 - Guess my number!
 
+/* 
+//////////////////////////////////////////////////////////////////////////////////////////
 // Selecting an element - accessing it by html class name
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct number!';
@@ -13,6 +15,18 @@ document.querySelector('.score').textContent = 20 ;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+ */
 
+//////////////////////////////////////////////////////////////////////////////////////////
+// Handling click events
 
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+  console.log(typeof guess);
+  // document.querySelector('.message').textContent = 'Correct number!';
 
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Insert a number please 😊';
+  }
+});
