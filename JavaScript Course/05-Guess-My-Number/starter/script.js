78f8947f3922fa1388a60d3e394e680dc57cb8d9
Ardit Zubaku <zubaku92@gmail.com>
2023-04-19 12:26:12 +0200

    Selecting and manipulating elements.

--- JavaScript Course/05-Guess-My-Number/starter/script.js
+++ JavaScript Course/05-Guess-My-Number/starter/script.js
@@ -3,5 +3,16 @@
 //////////////////////////////////////////////////////////////////////////////////////////
 // Project #1 - Guess my number!
 
-// Selecting an element
+// Selecting an element - accessing it by html class name
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20 ;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+
+
