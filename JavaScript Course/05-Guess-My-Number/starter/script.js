ff492973836b2c745a23cd8e81f9599874196faa
Ardit Zubaku <zubaku92@gmail.com>
2023-04-19 13:01:12 +0200

    Implementing the game logic.

--- JavaScript Course/05-Guess-My-Number/starter/script.js
+++ JavaScript Course/05-Guess-My-Number/starter/script.js
@@ -20,13 +20,43 @@ console.log(document.querySelector('.guess').value);
 //////////////////////////////////////////////////////////////////////////////////////////
 // Handling click events
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1; // We add +1 to compensate the removal
+// of the decimal part that trunc() does
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
+//////////////////////////////////////////////////////////////////////////////////////////
+// Implementing the game logic
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
   console.log(typeof guess);
-  // document.querySelector('.message').textContent = 'Correct number!';
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'Insert a number please 😊';
+    document.querySelector('.message').textContent =
+      'Insert a number please 😊';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      score--;
+      document.querySelector('.message').textContent = 'Too high!';
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You lost the game. Try again!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      score--;
+      document.querySelector('.message').textContent = 'Too low!';
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'You lost the game. Try again!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
