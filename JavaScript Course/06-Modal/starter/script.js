c587123c7854b636b3688a3699e6866ddd3ccd29
Ardit Zubaku <zubaku92@gmail.com>
2023-04-20 19:05:37 +0200

    Project #2 - Modal.

--- JavaScript Course/06-Modal/starter/script.js
+++ JavaScript Course/06-Modal/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+//////////////////////////////////////////////////////////////////////////////////////////
+// Project #2 - Modal
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.close-modal');
+const showButton = document.querySelectorAll('.show-modal'); // returns a NodeList
+
+for (let i = 0; i < showButton.length; i++) {
+  console.log(showButton[i].textContent);
+}
