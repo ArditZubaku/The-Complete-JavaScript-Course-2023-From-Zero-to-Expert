83353b15e72451ac907ae643e7080186a0503892
Ardit Zubaku <zubaku92@gmail.com>
2023-04-23 00:53:49 +0200

    Rolling the dice.

--- JavaScript Course/07-Pig-Game/starter/script.js
+++ JavaScript Course/07-Pig-Game/starter/script.js
@@ -4,10 +4,34 @@
 const score0Element = document.querySelector('#score--0');
 // Faster way
 const score1Element = document.getElementById('score--1');
-
+const current0Element = document.getElementById('current--0')
 const diceElement = document.querySelector('dice');
 
+const buttonNew = document.querySelector('.btn--new');
+const buttonRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
+
+// Starting conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
-
 diceElement.classList.add('hidden');
+let currentScore = 0;
+
+// Rolling dice functionality
+buttonRoll.addEventListener('click', () => {
+  //1. Generate a random dice roll
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display the dice
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice-${diceNumber}.png`;
+
+  // 3. Check for rolled '1': if true,
+  if (diceNumber !== 1) {
+    // Add dice to the current score
+    currentScore += diceNumber;
+
+  } else {
+    // switch to the next player
+  }
+});
