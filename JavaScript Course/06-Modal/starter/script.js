eda4a45e0a90fa7e5f0d2eae43d32d83626e6633
Ardit Zubaku <zubaku92@gmail.com>
2023-04-22 21:47:40 +0200

    Working with classes.

--- JavaScript Course/06-Modal/starter/script.js
+++ JavaScript Course/06-Modal/starter/script.js
@@ -5,9 +5,23 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const closeButton = document.querySelector('.close-modal');
-const showButton = document.querySelectorAll('.show-modal'); // returns a NodeList
+const closeModalButton = document.querySelector('.close-modal');
+const showModalButton = document.querySelectorAll('.show-modal'); // returns a NodeList
 
-for (let i = 0; i < showButton.length; i++) {
-  console.log(showButton[i].textContent);
+const openModal = () => {
+  console.log('Button clicked');
+  modal.classList.remove('hidden'); // we can pass multiple args seperated by commas
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < showModalButton.length; i++) {
+  // console.log(showModalButton[i].textContent);
+  showModalButton[i].addEventListener('click', openModal); // we only declare it, so the click calls it 
 }
+
+closeModalButton.addEventListener('click', closeModal); // method is not called using () bc then JS would execute the method whenever reached the line, and not based on the click invocation
+overlay.addEventListener('click', closeModal);
