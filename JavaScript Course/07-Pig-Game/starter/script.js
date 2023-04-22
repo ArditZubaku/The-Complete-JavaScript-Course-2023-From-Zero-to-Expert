1478ae8af3a475ba4bf812f8b5626b36166bc5ae
Ardit Zubaku <zubaku92@gmail.com>
2023-04-22 22:53:16 +0200

    Project #3 - Pig Game.

--- JavaScript Course/07-Pig-Game/starter/script.js
+++ JavaScript Course/07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// Selecting by id
+const score0Element = document.querySelector('#score--0');
+// Faster way
+const score1Element = document.getElementById('score--1');
+
+const diceElement = document.querySelector('dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+
+diceElement.classList.add('hidden');
