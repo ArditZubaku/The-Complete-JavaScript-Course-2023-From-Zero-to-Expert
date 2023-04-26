b12861cffbda4b52bba35bdf1188842077fba6c3
Ardit Zubaku <zubaku92@gmail.com>
2023-04-26 12:20:55 +0200

    The "this" keyword in practice.

--- JavaScript Course/08-Behind-the-Scenes/starter/script.js
+++ JavaScript Course/08-Behind-the-Scenes/starter/script.js
@@ -48,7 +48,7 @@ calcAge(1991);
 // console.log(age);
 // printAge();
 */
-
+/* 
 //////////////////////////////////////////////////////////////////////////////////
 // Variable environment: Hoisting and the TDZ.
 
@@ -103,5 +103,46 @@ const z = 3;
 
 console.log(x === window.x);
 console.log(y === window.y);
+*/
 
 //////////////////////////////////////////////////////////////////////////////////
+// The THIS keyword
+
+// Takes the value of (points to) the "owner" of the function
+// in which the this keyword is used.
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2023 - birthYear);
+  console.log(this); // Gets own this keyword even tho === undefined
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2023 - birthYear);
+  console.log(this); // Doesn't get own this keyword, inherits parent's
+};
+
+calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this); // Obj itself === the owner of the method
+    console.log(2023 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2007,
+};
+
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
