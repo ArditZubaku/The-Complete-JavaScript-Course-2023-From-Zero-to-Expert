adea569b96c690de6dc8474ec59a70c74d0f0162
Ardit Zubaku <zubaku92@gmail.com>
2023-05-09 03:54:18 +0200

    The bind method.

--- JavaScript Course/10-Functions/starter/script.js
+++ JavaScript Course/10-Functions/starter/script.js
@@ -141,7 +141,7 @@ const greet2 = greeting => name => console.log(`${greeting} ${name}`);
 console.log(greet2('Test 2'));
 greet2('Hello')('Jonas 2');
 */
-
+/* 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The call and apply methods
 
@@ -199,3 +199,104 @@ console.log(swiss);
 // Doing APPLY method way with CALL method
 book.call(swiss, ...arrayOfData);
 console.log(swiss);
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The bind method
+
+// This method returns a new function, doesn't immediately call the function
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  // book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+    );
+    this.bookings.push({
+      flight: `${this.iataCode}${flightNum}`,
+      name,
+    });
+  },
+};
+
+const book = lufthansa.book;
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const swiss = {
+  airline: 'Swiss',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+// book.call(eurowings, 123, 'Test');
+
+// Using the bind method:
+const bookEurowings = book.bind(eurowings); // Return a new function in which "this" will always point to the passed object
+const bookLufthansa = book.bind(lufthansa);
+const bookSwiss = book.bind(swiss);
+
+bookEurowings(123, 'Steven Williams');
+console.log(eurowings);
+
+// Can pass multiple args which then will be used as pre-defined
+
+const bookEurowings23 = book.bind(eurowings, 23);
+bookEurowings23(123, 'Martha Williams'); // The second argument wasnt taken at all cus now it takes only one argument since we pre-set the first one
+bookEurowings23('John Williams');
+console.log(eurowings);
+
+// With Event listeners
+lufthansa.planes = 300;
+lufthansa['buyPlane'] = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+
+console.log(lufthansa);
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+// In this case the this keyword is the button itself, since we know that the this keyword belongs to the 'element' that calls the function.
+// We use the bind method to fix this problem, bc this method returns a new function, and we need to pass a function, not call it.
+
+// Partial application - pre-setting parameters
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23); // We set the this keyword to null since its use is not in our interest in this case
+// addVAT = value => value + value * 0.23;
+console.log(addVAT(100));
+
+const addVAT2 = (value, rate = 0.23) => value + value * rate;
+console.log(addVAT2(100));
+
+// Challenge - achieve the same thing as we did with the bind method but now using callbacks
+
+const addVAT3 =
+  (rate = 0.23) =>
+  value =>
+    value + value * rate;
+
+console.log(addVAT3(/* 0.24 */)(100));
+
+// Jonas' solution
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT4 = addTaxRate(0.23);
+console.log(addVAT4(100));
