cfbf2dba5ed9f29dde0d23a26a537df4f9bbf0fd
Ardit Zubaku <zubaku92@gmail.com>
2023-05-09 07:59:56 +0200

    Coding Challenge #15.

--- JavaScript Course/10-Functions/starter/script.js
+++ JavaScript Course/10-Functions/starter/script.js
@@ -201,6 +201,7 @@ book.call(swiss, ...arrayOfData);
 console.log(swiss);
 */
 
+/* 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // The bind method
 
@@ -289,7 +290,7 @@ const addVAT3 =
   value =>
     value + value * rate;
 
-console.log(addVAT3(/* 0.24 */)(100));
+console.log(addVAT3()(100));
 
 // Jonas' solution
 const addTaxRate = function (rate) {
@@ -300,3 +301,100 @@ const addTaxRate = function (rate) {
 
 const addVAT4 = addTaxRate(0.23);
 console.log(addVAT4(100));
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #15.
+
+/* 
+Coding Challenge #1
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an
+array with the number of replies for each option. This data is stored in the starter
+'poll' object below.
+
+Your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The
+method does 2 things:
+    1.1.
+    Display a prompt window for the user to input the number of the
+    selected option. The prompt should look like this:
+    What is your favourite programming language?
+    0: JavaScript
+    1: Python
+    2: Rust
+    3: C++
+    (Write option number)
+        1.2.
+        Based on the input number, update the 'answers' array property. For
+        example, if the option is 3, increase the value at position 3 of the array by 1. 
+        Make sure to check if the input is a number and if the number makes
+        sense (e.g. answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The
+method takes a string as an input (called 'type'), which can be either 'string'
+or 'array'. If type is 'array', simply display the results array as it is, using
+console.log(). This should be the default option. If type is 'string', display a
+string like "Poll results are 13, 2, 4, 1".
+4. Run the 'displayResults' method at the end of each
+'registerNewAnswer' method call.
+5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+object! So what should the this keyword look like in this situation?
+
+Test data for bonus:
+*Data 1: [5, 2, 3]
+*Data 2: [1, 5, 3, 9, 6, 1]
+Hints: Use many of the tools you learned about in this and the last section 
+GOOD LUCK 
+*/
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  // This generates [0, 0, 0, 0]. More in the next section!
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    const favLang = Number(
+      prompt(`${this.question}\n${this.options.join(
+        '\n'
+      )}\n(Write option number)
+  
+    `)
+    );
+
+    // if (typeof favLang === 'number' && favLang >= 0 && favLang <= 3) {
+    //   this.answers[favLang]++;
+    // }
+
+    typeof favLang === 'number' &&
+      favLang < this.answers.length &&
+      this.answers[favLang]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll)); // when working with eventHandlers or even higher tier functions, always use the bind method
+
+// *Data 1: [5, 2, 3]
+// *Data 2: [1, 5, 3, 9, 6, 1]
+
+const data_1 = {
+  answers: [5, 2, 3], // the object shall contain this property since the method calls this property
+};
+
+// poll.displayResults.call({ answers: [5, 2, 3] });
+poll.displayResults.call(data_1, 'string');
