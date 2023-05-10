d2ea06c07cba2e0264378c3a8ea87d3a24cd07a5
Ardit Zubaku <zubaku92@gmail.com>
2023-05-10 13:44:02 +0200

    Coding Challenge #16.

--- JavaScript Course/10-Functions/starter/script.js
+++ JavaScript Course/10-Functions/starter/script.js
@@ -459,6 +459,7 @@ booker();
 console.dir(booker);
 */
 
+/* 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // More closure examples.
 
@@ -504,4 +505,29 @@ const boardPassengers = function (n, wait) {
 // Closure has priority over the scope chain
 const perGroup = 1999;
 
-boardPassengers(180, 3);
+boardPassengers(180, 3); 
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #16.
+
+/* 
+This is more of a thinking challenge than a coding challeng e
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  // Has the variable in the backpack
+  document.querySelector('body').addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
