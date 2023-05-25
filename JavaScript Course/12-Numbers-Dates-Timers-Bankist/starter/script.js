9aad75dd26347c0b747af6b0fe5b96029c114f43
ArditZubaku <zubaku92@gmail.com>
2023-05-25 11:34:33 +0200

    Operations with dates.

--- JavaScript Course/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ JavaScript Course/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -81,6 +81,25 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  // Day/Month/Year
+  const day = `${date.getDate()}`.padStart(2, 0);
+  const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -92,12 +111,8 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
 
     const date = new Date(acc.movementsDates[i]);
-    // Day/Month/Year
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
 
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -494,3 +509,19 @@ console.log(Date.now()); // Current timestamp
 future.setFullYear(2040);
 console.log(future);
 */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Operations with dates.
+
+const future = new Date(2037, 10, 19, 15, 23);
+// console.log(Number(future));
+console.log(+future);
+
+const calcDaysPassed = (date1, date2) =>
+  Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+const days1 = calcDaysPassed(
+  new Date(2037, 3, 14),
+  new Date(2037, 3, 24, 10, 8)
+);
+console.log(days1);
