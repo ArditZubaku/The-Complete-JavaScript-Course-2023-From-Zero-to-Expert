0e6198aec20e2052a37a1a5c6625ddc5c2e5f592
ArditZubaku <zubaku92@gmail.com>
2023-06-14 09:57:22 +0200

    Styles, attributes and classes.

--- JavaScript Course/13-Advanced-DOM-Bankist/starter/script.js
+++ JavaScript Course/13-Advanced-DOM-Bankist/starter/script.js
@@ -30,6 +30,7 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+/*
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Selecting, creating and deleting elements
 
@@ -39,32 +40,111 @@ console.log(document.head);
 console.log(document.body);
 
 const header = document.querySelector('.header');
-console.log(header);
-const allSelections = document.querySelectorAll('.section');
-// console.log(allSelections);
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
 
 document.getElementById('section--1');
 const allButtons = document.getElementsByTagName('button');
-// console.log(allButtons);
+console.log(allButtons);
 
-// console.log(document.getElementsByClassName('btn'));
+console.log(document.getElementsByClassName('btn'));
 
 // Creating and inserting elements
 const message = document.createElement('div');
 message.classList.add('cookie-message');
-message.textContent =
-  'We use cookies for improved functionality and analytics.';
+// message.textContent = 'We use cookies for improved functionality and analytics.';
 message.innerHTML =
   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-// header.prepend(message); // Adds as the first child of the header
-header.append(message); // Adds as the last child of the header
+
+// header.prepend(message);
+header.append(message);
 // header.append(message.cloneNode(true));
-header.before(message);
-header.after(message);
+
+// header.before(message);
+// header.after(message);
 
 // Delete elements
 document
   .querySelector('.btn--close-cookie')
   .addEventListener('click', function () {
-    message.remove();
+    // message.remove();
+    message.parentElement.removeChild(message);
   });
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Styles, attributes and classes
+
+// Styles
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+message.style.backgroundColor = '#37383d'; // Gets added as inline style.
+message.style.width = '120%'; // Write it exactly as you would do in CSS.
+
+console.log(message.style.height); // Only works for the set ones.
+console.log(message.style.backgroundColor);
+console.log(message.style.color); // Even though it exists in the stylesheet, we didn't set it via code.
+
+// To get those styles:
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src); // Works with only standard attributes
+
+// Setting the values
+logo.alt = 'Beautiful minimalist logo';
+
+// console.log(logo.designer); // Non-standard
+console.log(logo.getAttribute('designer'));
+
+// Setting the attributes
+logo.setAttribute('company', 'Bankist');
+
+// Getting the attributes
+console.log(logo.src); // Absolute path
+console.log(logo.getAttribute('src')); // Relative path
+
+const link = document.querySelector('.twitter-link');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+const link2 = document.querySelector('.nav__link--btn');
+console.log(link2.getAttribute('href'));
+
+// Data attributes - attributes that start with data - are stored in the dataset object
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use - overrides all the existing classes
+// logo.className = 'test'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
