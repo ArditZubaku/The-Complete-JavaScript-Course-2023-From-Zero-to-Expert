bd47d6108e0f95f0462c5744f027b57b9492d080
ArditZubaku <zubaku92@gmail.com>
2023-06-15 15:28:59 +0200

    Constructor functions and the "new" operator.

--- JavaScript Course/14-OOP/starter/script.js
+++ JavaScript Course/14-OOP/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Constructor functions and the "new" operator.
 
 // Convention: constructor functions start with uppercase letter.
 // Only function declarations and function expressions are allowed, arrow functions are not bc of this keyword.
@@ -33,3 +36,40 @@ const test = 'Test';
 
 console.log(matilda instanceof Person);
 console.log(test instanceof Person);
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Prototypes.
+
+// Each and every function in JS has a property called 'prototype'.
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const jonas = new Person('Jonas', 1991);
+const matilda = new Person('Matilda', 2007);
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+// Now, all the objects created by the "Person" constructor will get access to the methods of the prototype property.
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(Person));
+// So, by this we can conclude that jonas is not prototype of Person, Person is prototype of jonas.
+
+// Setting properties on the prototype:
+Person.prototype.species = 'Humans'; // Not own property of the object
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
