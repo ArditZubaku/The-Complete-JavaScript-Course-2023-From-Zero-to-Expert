75ec41cb5496c5cd6fe713b38b1125c120d1e62e
ArditZubaku <zubaku92@gmail.com>
2023-06-22 14:00:00 +0200

    Displaying a map using Leaflet library.

--- JavaScript Course/15-Mapty/starter/script.js
+++ JavaScript Course/15-Mapty/starter/script.js
@@ -1,15 +1,27 @@
 'use strict';
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+// const form = document.querySelector('.form');
+// const containerWorkouts = document.querySelector('.workouts');
+// const inputType = document.querySelector('.form__input--type');
+// const inputDistance = document.querySelector('.form__input--distance');
+// const inputDuration = document.querySelector('.form__input--duration');
+// const inputCadence = document.querySelector('.form__input--cadence');
+// const inputElevation = document.querySelector('.form__input--elevation');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Using the Geolocation API.
@@ -20,8 +32,27 @@ if (navigator.geolocation)
       const { latitude, longitude } = position.coords;
       // console.log(latitude, longitude);
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+      // Displaying a map using Leaflet library.
+
+      // 'map' is the id of a html element that will hold/display the map
+
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 20); // 20 = zoom level
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => {
       alert("Couldn't get current position");
     }
   );
+
+console.log(firstName);
