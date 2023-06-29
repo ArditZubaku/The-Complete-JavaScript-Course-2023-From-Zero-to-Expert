d35573f5bc8db2391fe2ac53cccf8c6ef5b02c3d
ArditZubaku <zubaku92@gmail.com>
2023-06-29 14:14:50 +0200

    Welcome to callback hell.

--- JavaScript Course/16-Asynchronous/starter/script.js
+++ JavaScript Course/16-Asynchronous/starter/script.js
@@ -12,16 +12,9 @@ const countriesContainer = document.querySelector('.countries');
 
 // Old school way
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const html = `
-        <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
           <img class="country__img" src="${data.flags.png}"  alt="State flag"/>
           <div class="country__data"> 
             <h3 class="country__name">${data.name.common}</h3>
@@ -46,18 +39,56 @@ const getCountryData = function (country) {
         </article>
                     `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbor country (2)
+    const neighbour = data.borders?.[0];
+
+    // AJAX call country 1
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      // const [data2] = JSON.parse(request2.responseText);
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      // Render country 2
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('kosovo');
-getCountryData('albania');
-getCountryData('usa');
-getCountryData('germany');
+getCountryAndNeighbour('kosovo');
+// getCountryAndNeighbour('albania');
+// getCountryAndNeighbour('usa');
+// getCountryAndNeighbour('germany');
 
-const data = {
-  languages: {
-    eng: 'English',
-  },
-};
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
