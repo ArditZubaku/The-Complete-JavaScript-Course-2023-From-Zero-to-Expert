115c94d39f6d2bf7c26ab2b405fdf13ad20e401c
ArditZubaku <zubaku92@gmail.com>
2023-07-05 16:27:57 +0200

    Coding Challenge #27.

--- JavaScript Course/16-Asynchronous/starter/script.js
+++ JavaScript Course/16-Asynchronous/starter/script.js
@@ -243,7 +243,8 @@ console.log('Test end');
 // The timeout will be put in the callback queue.
 */
 
-/*////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Building a simple promise.
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log('Lottery draw is happening!!!');
@@ -707,3 +708,122 @@ Promise.any([Promise.resolve('Success 2'), Promise.reject('Failed 2')])
   .catch(err => {
     console.error(err);
   });
+
+/*
+The differences between  Promise.all ,  Promise.race , and  Promise.any  are as follows: 
+1.  Promise.all : Returns a Promise that fulfills with an array of values when all the Promises 
+in the input iterable have fulfilled. If any of the Promises reject, the returned Promise will reject with the reason 
+of the first rejected Promise. 
+2.  Promise.race : Returns a Promise that fulfills or rejects as soon as any of the Promises 
+in the input iterable settles (fulfills or rejects). The returned Promise adopts the settled state 
+(fulfilled or rejected) of the first settled Promise in the input iterable. 
+3.  Promise.any : Returns a Promise that fulfills as soon as any of the Promises in the input iterable fulfills. 
+If all the Promises reject, the returned Promise will reject with an AggregateError containing all the rejection reasons 
+In summary, Promise.all waits for all Promises to fulfill, Promise.race settles as soon as any Promise settles, 
+and Promise.any fulfills as soon as any Promise fulfills.
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding Challenge #27
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+PART 1
+Write an async function 'loadNPause' that recreates Coding Challenge #2, this time using async/await 
+(only the part where the promise is consumed). Compare the two versions, think about the big differences, 
+and see which one you like more.
+Don't forget to test the error handler, and to set the network speed to 'Fast 3G' in the dev tools Network tab.
+
+PART 2
+1. Create an async function 'loadAll' that receives an array of image paths 'imgArr';
+2. Use .map to loop over the array, to load all the images with the 'createImage' 
+function (call the resulting array 'imgs')
+3. Check out the 'imgs' array in the console! Is it like you expected?
+4. Use a promise combinator function to actually get the images from the array 
+5. Add the 'parallel' class to all the images (it has some CSS styles).
+
+TEST DATA: ['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']. To test, turn off the 'loadNPause' function.
+
+GOOD LUCK 
+*/
+
+const imgContainer = document.querySelector('.images');
+
+const createImage = imgPath => {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement('img');
+    img.src = imgPath;
+
+    img.addEventListener('load', () => {
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error', () => {
+      reject(new Error(`Image not found at ${imgPath}`));
+    });
+  });
+};
+//
+// let currentImg;
+//
+// createImage('img/img-1.jpg')
+//   .then(resolvedValue => {
+//     currentImg = resolvedValue;
+//     console.log('Image 1 loaded');
+//     return wait(4);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//     return createImage('img/img-2.jpg');
+//   })
+//   .then(resolvedValue => {
+//     currentImg = resolvedValue;
+//     console.log('Image 2 loaded');
+//     return wait(4);
+//   })
+//   .then(() => {
+//     currentImg.style.display = 'none';
+//   })
+//   .catch(err => {
+//     console.error(err);
+//   });
+
+// PART 1:
+
+const loadNPause = async () => {
+  try {
+    // Load image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log(`Image 1 loaded`);
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load image 2
+    img = await createImage('img/img-1.jpg');
+    console.log(`Image 2 loaded`);
+    await wait(2);
+    img.style.display = 'none';
+  } catch (e) {
+    console.error(e);
+  }
+};
+// loadNPause();
+
+// PART 2:
+const loadAll = async imgArray => {
+  try {
+    const imgs = imgArray.map(async img => await createImage(img));
+    console.log(imgs);
+
+    const resolvedImgs = await Promise.all(imgs);
+    console.log(resolvedImgs);
+
+    resolvedImgs.forEach(img => img.classList.add('parallel'));
+  } catch (e) {
+    console.log(e);
+  }
+};
+loadAll(['img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg']);
