6377e19765e705cae2711164f451bf70554303a2
Ardit Zubaku <ardit.zubaku@telos-labs.com>
2023-07-20 13:16:31 +0200

    Helpers and configuration files.

--- JavaScript Course/18-forkify/starter/src/js/model.js
+++ JavaScript Course/18-forkify/starter/src/js/model.js
@@ -1,21 +1,16 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async id => {
   try {
-    const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-
-    console.log(res, data);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
+
     state.recipe = {
       id: recipe.id,
       title: recipe.title,
@@ -29,6 +24,6 @@ export const loadRecipe = async id => {
 
     console.log(state.recipe);
   } catch (e) {
-    alert(e);
+    console.error(`Temporary error handling: ${e} `);
   }
 };
